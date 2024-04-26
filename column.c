adeab63ad508973af8a47af596a73955d8b35b95
Maxime <maxime.berger@efrei.net>
2024-04-26 13:16:56 +0200

    fonction insert value

--- column.c
+++ column.c
@@ -12,3 +12,29 @@ COLUMN *create_column(char *title){
     return column;
 }
 
+int insert_value(COLUMN *col, int value){
+    if (col->data == NULL){
+        col->data = (int*)malloc(REALOC_SIZE*sizeof(int));
+        if (col->data == NULL){
+            return 0;
+        }
+        else{
+            col->PS = 256;
+        }
+    }
+    else if(col->LS >= col->PS ){
+        int* ptr = col->data;
+        col->data = (int*)realloc(col->data, REALOC_SIZE*sizeof(int) + col->PS);
+        if (col->data == NULL){
+            col->data = ptr;
+            return 0;
+        }
+        else{
+            col->PS += 256;
+        }
+    }
+    col->data[col->LS] = value;
+    col->LS += 1;
+    return 1;
+}
+
